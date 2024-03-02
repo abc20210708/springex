aa6dfb9cae866f3724a743530a6171a2dbcc43ff
abc20210708 <gotqlc2670@naver.com>
2024-03-02 10:49:39 +0900

    [24.03.02(토)] p.233 에러발생 - 자동 주입된 멤버는 올바른 Spring bean에서 정의

--- src/main/java/org/zerock/springex/sample/SampleService.java
+++ src/main/java/org/zerock/springex/sample/SampleService.java
@@ -1,4 +1,13 @@
 package org.zerock.springex.sample;
 
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ToString
 public class SampleService {
+
+    //@Autowired
+    // 자동 주입된 멤버는 올바른 Spring bean에서 정의되어야 합니다
+    private SampleDAO sampleDAO;
+
 }
