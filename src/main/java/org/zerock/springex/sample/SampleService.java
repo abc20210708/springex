ae220bf1b639afa576754b383688b12e9df92517
abc20210708 <gotqlc2670@naver.com>
2024-03-04 21:31:10 +0900

    [24.03.04(월)] @Service 추가 에러 우선 해결

--- src/main/java/org/zerock/springex/sample/SampleService.java
+++ src/main/java/org/zerock/springex/sample/SampleService.java
@@ -2,12 +2,15 @@ package org.zerock.springex.sample;
 
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @ToString
+@Service
 public class SampleService {
 
-    //@Autowired
+    @Autowired
     // 자동 주입된 멤버는 올바른 Spring bean에서 정의되어야 합니다
+    //https://dololak.tistory.com/564 참고 후 에러는 우선 해결됨
     private SampleDAO sampleDAO;
 
 }
