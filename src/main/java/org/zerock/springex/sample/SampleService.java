c1bd6c5d8e529411519b977462b1238faf25c465
abc20210708 <gotqlc2670@naver.com>
2024-03-05 21:28:05 +0900

    [24.03.05(화)] p.236 생성자 주입 방식

--- src/main/java/org/zerock/springex/sample/SampleService.java
+++ src/main/java/org/zerock/springex/sample/SampleService.java
@@ -1,11 +1,13 @@
 package org.zerock.springex.sample;
 
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @ToString
 @Service
+@RequiredArgsConstructor
 public class SampleService {
 
     @Autowired
