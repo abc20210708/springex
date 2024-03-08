039731841952d5158159084ad9380add42f07678
abc20210708 <gotqlc2670@naver.com>
2024-03-08 23:35:20 +0900

    [24.03.08(금)] @Qualifier 이용하기

--- src/main/java/org/zerock/springex/sample/SampleService.java
+++ src/main/java/org/zerock/springex/sample/SampleService.java
@@ -3,6 +3,7 @@ package org.zerock.springex.sample;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @ToString
@@ -10,7 +11,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SampleService {
 
-    @Autowired
+    @Qualifier("normal")
+    //@Autowired
     // 자동 주입된 멤버는 올바른 Spring bean에서 정의되어야 합니다
     //https://dololak.tistory.com/564 참고 후 에러는 우선 해결됨
     private SampleDAO sampleDAO;
