85cf2a32fa542c21d34d3f39c1ca1e53f75a8eb1
abc20210708 <gotqlc2670@naver.com>
2024-04-16 21:08:03 +0900

    [24.04.16(화)] No mapping 에러

--- src/main/java/org/zerock/springex/controller/TodoController.java
+++ src/main/java/org/zerock/springex/controller/TodoController.java
@@ -4,6 +4,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
 @Controller
 @Log4j2
 public class TodoController {
@@ -38,6 +40,12 @@ public class TodoController {
         log.info("name: " + name);
         log.info("age: " + age);
 
+    }
+
+    @GetMapping("/ex3")
+    public void ex3(LocalDate dueDate) {
+        log.info("ex3 ===");
+        log.info("dueDate : " + dueDate);
     }
 
 }
