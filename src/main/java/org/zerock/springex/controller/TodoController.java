e7eb932d4cc7f02d024e2da14356e90cf7f30308
abc20210708 <gotqlc2670@naver.com>
2024-04-17 20:42:54 +0900

    [24.04.17(수)] WARN [org.springframework.web.servlet.PageNotFound] No mapping for GET /

--- src/main/java/org/zerock/springex/controller/TodoController.java
+++ src/main/java/org/zerock/springex/controller/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
+@RequestMapping("/todo")
 @Controller
 @Log4j2
 public class TodoController {
@@ -26,26 +27,6 @@ public class TodoController {
         log.info("POST todo register...");
     }
 
-    @GetMapping("/ex1")
-    public void ex1(String name, int age) {
-        log.info("== ex1 ==");
-        log.info(" == name:  "+ name);
-        log.info(" == age:  "+ age);
-    }
 
-    @GetMapping("/ex2")
-    public void ex2(@RequestParam(name = "name", defaultValue = "AAA") String name,
-                    @RequestParam(name = "age", defaultValue = "20") int age) {
-        log.info("ex2 ===========");
-        log.info("name: " + name);
-        log.info("age: " + age);
-
-    }
-
-    @GetMapping("/ex3")
-    public void ex3(LocalDate dueDate) {
-        log.info("ex3 ===");
-        log.info("dueDate : " + dueDate);
-    }
 
 }
