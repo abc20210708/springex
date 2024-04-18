a245b31bcfb540a6c81ebd1129790383194572ed
abc20210708 <gotqlc2670@naver.com>
2024-04-18 19:57:44 +0900

    [24.04.18(목)] 범용적인 예외처리

--- src/main/java/org/zerock/springex/controller/exception/CommonExceptionAdvice.java
+++ src/main/java/org/zerock/springex/controller/exception/CommonExceptionAdvice.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+
 @ControllerAdvice
 @Log4j2
 public class CommonExceptionAdvice {
@@ -18,4 +20,25 @@ public class CommonExceptionAdvice {
 
         return "NUMBER FORMAT EXCEPTION";
     }
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public String exceptCommon(Exception exception) {
+
+        log.error("-------------------------------");
+        log.error(exception.getMessage());
+
+        StringBuffer buffer = new StringBuffer("<ul>");
+
+        buffer.append("<li>"+exception.getMessage()+"</li>");
+
+        Arrays.stream(exception.getStackTrace()).forEach(stackTraceElement -> {
+            buffer.append("<li>"+stackTraceElement+"</li>");
+        });
+        buffer.append("<ul>");
+
+        return buffer.toString();
+    }
+
+
 }
