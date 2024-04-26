6993caaefd63b0287fe1990cc18b13dc3f31a0ec
abc20210708 <gotqlc2670@naver.com>
2024-04-26 21:36:28 +0900

    [24.04.26(금)] 한글 처리를 위한 필터 설정

--- src/main/java/org/zerock/springex/controller/TodoController.java
+++ src/main/java/org/zerock/springex/controller/TodoController.java
@@ -2,6 +2,7 @@ package org.zerock.springex.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.springex.dto.TodoDTO;
@@ -14,7 +15,7 @@ import java.time.LocalDate;
 public class TodoController {
 
     @RequestMapping("/list")
-    public void list(){
+    public void list(Model model){
         log.info("todo list! ");
     }
 
