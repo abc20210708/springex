b3c6fab7cd10085f6cef3f2f8ab28da899c85f23
abc20210708 <gotqlc2670@naver.com>
2024-05-24 21:12:44 +0900

    [24.05.24(금)] p.362 Servlet.init() 호출이 예외를 발생

--- src/main/java/org/zerock/springex/controller/TodoController.java
+++ src/main/java/org/zerock/springex/controller/TodoController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.springex.dto.PageRequestDTO;
 import org.zerock.springex.dto.TodoDTO;
 import org.zerock.springex.service.TodoService;
 
@@ -22,10 +23,15 @@ public class TodoController {
     private final TodoService todoService;
 
     @RequestMapping("/list")
-    public void list(Model model){
-        log.info("todo list! ");
+    public void list(@Valid PageRequestDTO pageRequestDTO,
+                    BindingResult bindingResult ,Model model){
 
-        //model.addAttribute("dtoList", todoService.getList());
+        log.info(pageRequestDTO);
+
+        if (bindingResult.hasErrors()) {
+            pageRequestDTO = PageRequestDTO.builder().build();
+        }
+        model.addAttribute("responseDTO", todoService.getList(pageRequestDTO));
     }
 
     //@RequestMapping(value = "/register", method = RequestMethod.GET)
